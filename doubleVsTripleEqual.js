5c8990e363eb975355a114fb92c9cff8ad7e2c99
Sabbir Ahmed Shawon <shawon.mu11@gmail.com>
2021-02-05 19:51:47 +0600

    double vs triple equal

---
+++ doubleVsTripleEqual.js
@@ -0,0 +1,19 @@
+//== does not check variable type
+//=== check variable value and variable type
+// const a=5;
+// const b="5";
+// if(a==b){
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+
+const a=0;
+const b=false;
+if(a===b){
+    console.log('true');
+}
+else {
+    console.log('false');
+}
