ff70e7f9e876c1b3583ffe9dc3d8f591bc66768f
Sabbir Ahmed Shawon <shawon.mu11@gmail.com>
2021-02-05 19:04:31 +0600

    truthy falsy

---
+++ truth-false.js
@@ -0,0 +1,23 @@
+//-----------
+//falsy value
+//0
+//""
+//undefined variable
+//null
+//NaN
+//False
+//------------
+//truthy value
+//'0'
+//" "
+//[]
+//{}
+//'False'
+
+const name='78'
+if(name.length>0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition id false");
+}
