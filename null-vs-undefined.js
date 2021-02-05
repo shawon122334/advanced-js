ffe9037d353f6684940703917352e6b8193d35d9
Sabbir Ahmed Shawon <shawon.mu11@gmail.com>
2021-02-05 19:36:10 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,29 @@
+//1:if we do not assign a value to variable and try to work with it 
+//let name;
+//console.log(name);
+//------------------
+//2:if we do not return from function,and try to work outside of functio n with it 
+//2.1: if we only say return but do no say what to return
+// function add(num1,num2){
+//     console.log(num1+num2);
+// }
+//-------------------
+//3:if we pass two parameter but put 1 value 
+// function add(num2,num3){
+//     console.log(num2,num3);
+// }
+// const result=add(12);
+// console.log(result);
+//-------------------------
+//4:if we try to read a property of an object that is not available in the object
+// const name={a:'sabbir',b:'ahmed'};
+// console.log(name.c);
+//-------------------
+//5:we can not use reserve word
+let a = undefined;
+console.log(a);
+//--------------------
+//null
+//we can set a null value
+//we can get all use value and make them null
+
